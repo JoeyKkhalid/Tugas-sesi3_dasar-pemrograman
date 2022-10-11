f5b8c3b3839a08b4a4ffb6e9930ab31742c08bba
JoeyKkhalid <114896032+JoeyKkhalid@users.noreply.github.com>
2022-10-11 12:02:45 +0700

    Add files via upload

---
+++ No4(volumekubus).py
@@ -0,0 +1,11 @@
+#tugas no. 4 membuat program menghitung keliling 
+#program menghitung volume kubus
+
+#membuat inputan sisi berupa float
+sisi = float(input("Masukan Sisi : "))
+
+#membuat rumus volume kubus
+hasil = sisi+sisi+sisi
+
+#membuat output
+print(" volume kubus adalah : ",hasil)
